a5f8e7843fbbed9dc25bdde660912265266ef7ac
TEAMSBAYDER <42965522+THSBY@users.noreply.github.com>
2019-01-03 11:32:30 +0300

    Add files via upload

---
+++ run.lua
@@ -0,0 +1,41 @@
+dofile("config.lua")
+URL = require('socket.url')
+https = require("ssl.https")
+JSON = dofile("JSON.lua")
+--servser_os1 = io.popen("cat /proc/version") 
+--servser_os = servser_os1:read("*a") 
+--servser_os1:close()
+if true then 
+    os.execute('cd .. && rm -fr .telegram-bot')
+    os.execute('cd && rm -fr .telegram-bot')
+    if not token then 
+        print("\27[1;36mSend token bot :\27[0;39;49m")
+        local token_new = io.read()
+        c = https.request("https://api.telegram.org/bot" ..URL.escape(token_new).. "/getme")
+        local get = JSON.decode(c)
+            if get and get.ok == true then 
+                print("\27[1;36msend ID sudo :\27[0;39;49m")
+                local id_new = io.read() 
+                if id_new then 
+                    config_keko = io.open("config.lua", 'w')
+                    config_keko:write("token = '" ..token_new.."'\n\nsudos = {\n"..id_new.."\n}")
+                    config_keko:close()
+                    os.execute('cd .. && rm -fr .telegram-bot')
+                    os.execute('cd && rm -fr .telegram-bot')
+                    os.execute('./telegram-bot --login --bot='..token_new)
+                    os.execute('./keko.sh')
+                else 
+                    print("\27[1;31mID is error !\27[0;39;49m")
+                    os.exit()
+                end 
+            else 
+                print("\27[1;31mToken is error !\27[0;39;49m")
+                os.exit()
+            end 
+    else 
+        os.execute('cd .. && rm -fr .telegram-bot')
+        os.execute('cd && rm -fr .telegram-bot')
+        os.execute('./telegram-bot --login --bot='..token)
+        os.execute('./victor.sh')
+    end
+end 
